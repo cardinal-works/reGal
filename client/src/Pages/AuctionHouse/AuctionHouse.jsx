6ef86361020bb9594b66f93ba9d90d82eb5d2cca
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2021-05-08 10:48:38 -0700

    started boilerplate for auction house
    Co-authored-by: Danny <thestinx@gmail.com>

--- client/src/Pages/AuctionHouse/AuctionHouse.jsx
+++ client/src/Pages/AuctionHouse/AuctionHouse.jsx
@@ -42,7 +42,7 @@ const AuctionHouse = ({ web3 }) => {
 			<Container className="auction-house-container">
 				<Form>
 					<Form.Group controlId="formBasicEmail">
-						<Form.Label>Email address</Form.Label>
+						<Form.Label> </Form.Label>
 						<Form.Control type="email" placeholder="Enter email" />
 						<Form.Text className="text-muted">
 							We'll never share your email with anyone else.
