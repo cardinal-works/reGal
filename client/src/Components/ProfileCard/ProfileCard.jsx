40a79cea74c9ed705c7c9cc66b4c98d695061ee9
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2021-05-30 18:44:19 -0700

    restlyled profile page

---
+++ client/src/Components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,101 @@
+// ** MODULES
+import React, { Fragment, useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+// ** COMPONENTS
+import CornerRibbon from 'react-corner-ribbon';
+import Countdown from 'react-countdown';
+import {
+	Row,
+	Col,
+	Image,
+	Button,
+	Container,
+	Card,
+	ListGroup,
+	ListGroupItem,
+} from 'react-bootstrap';
+// ** STORE
+import UserStore from '../../Stores/UserStore';
+import { observer } from 'mobx-react-lite';
+
+const ProfileCard = ({
+	_id,
+	display_name,
+	wallet_id,
+	profile_image,
+	profile_featured_id,
+	bio,
+	profile_bg_color,
+}) => {
+	const userStore = useContext(UserStore);
+	const { loadUser, user, loadingInitial, submitting } = userStore;
+
+	useEffect(() => {
+		console.log();
+		loadUser(window.ethereum.selectedAddress);
+		// .then(console.log("hello", Object.fromEntries(nftRegistry)))
+	}, []);
+	// const nftStore = useContext(NftStore);
+	// const { updateNft, loadNft } = nftStore;
+	// const [currentEtherPrice, setCurrentEtherPrice] = useState(null);
+
+	// const handleLikeNft = () => {
+	// 	loadNft(_id).then((user) => updateNft({ ...nft, likes: nft.likes + 1 }));
+	// };
+
+	return (
+		<Fragment>
+			<Container className="profile-card-container pb-3">
+				<Card
+					// as={Link}
+					// to={{
+					// 	pathname: `/details/${nft_id}`,
+					// 	state: { nft_id: Number(nft_id) },
+					// }}
+					className="profile-item-card">
+					<Card.Img
+						className="profile-card-image"
+						variant="top"
+						src={profile_image}
+					/>
+				</Card>
+				<Container className="pl-1 pr-1">
+					<Row className="overlay-container">
+						<Col md={12} className="symbols-container pt-1 pb-1">
+							<div className="likes-div">
+								<span
+									className="text-white  text-green"
+									style={{ fontWeight: '900' }}>
+									<i
+										style={{ fontSize: '14px', color: '#e2c249' }}
+										className="fas fa-sign-language"></i>
+								</span>
+								<span className="pl-1">
+									<i className="far fa-star star "></i>
+								</span>
+							</div>
+						</Col>
+						<Col md={12} className="pt-1">
+							<span className="text-white creator-link">
+								{' @'}
+								{display_name}
+							</span>
+						</Col>
+						<Col className="pt-1 pb-1 text-white">{user.bio}</Col>
+						<Col md={12} className="pt-1 pb-1">
+							<div className="more-div text-white pb-1 text-end">
+								{user.wallet_id === wallet_id ? (
+									<i className="fad fa-pencil"></i>
+								) : (
+									<i className="far fa-ellipsis-h pl-3"></i>
+								)}
+							</div>
+						</Col>
+					</Row>
+				</Container>
+			</Container>
+		</Fragment>
+	);
+};
+
+export default observer(ProfileCard);
--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -3,7 +3,9 @@ import React, { useEffect, useState, Fragment, useContext } from 'react';
 import { Container, Row, Col, Image, Button, Form, FormFile } from 'react-bootstrap';
 import { Link, history } from 'react-router-dom';
 //Contracts
+import ProfileCard from '../../Components/ProfileCard';
 import ProfileNftDisplay from '../../Components/ProfileNftDisplay';
+import NftDisplay from '../../Components/NftDisplay';
 import UserStore from '../../Stores/UserStore';
 import NftStore from '../../Stores/NftStore';
 import { observer } from 'mobx-react-lite';
@@ -70,7 +72,7 @@ const Profile = (props, web3) => {
 	const userStore = useContext(UserStore);
 	const nftStore = useContext(NftStore);
 	const { loadUser, updateUser, user, loadingInitial, submitting } = userStore;
-	const { loadNfts, getAllNfts, nftRegistry } = nftStore;
+	const { loadNfts, getAllNfts, loadNft, nft, nftRegistry } = nftStore;
 	const [editMode, setEditMode] = useState(false);
 
 	const [userChanges, setUserChanges] = useState(
@@ -80,16 +82,6 @@ const Profile = (props, web3) => {
 			display_name: '',
 		}
 	);
-
-	useEffect(() => {
-		console.log()
-		loadUser(window.ethereum.selectedAddress)
-		.then((res) => {
-		loadNfts({ user_id: res._id });
-		setUserChanges(res);
-		});
-		// .then(console.log("hello", Object.fromEntries(nftRegistry)))
-	}, []);
 
 	useEffect(() => {
 		// console.log(nftRegistry);
@@ -220,206 +212,90 @@ const Profile = (props, web3) => {
 	};
 
 	useEffect(() => {
+		console.log();
+		loadUser(window.ethereum.selectedAddress).then((res) => {
+			loadNfts({ user_id: res._id });
+			setUserChanges(res);
+		});
+		loadNft(1);
+		// .then(console.log("hello", Object.fromEntries(nftRegistry)))
+	}, []);
+
+	useEffect(() => {
 		// console.log(loadingInitial);
 		// console.log(getAllNfts)
+		console.log(user);
 	}, [loadingInitial, user, getAllNfts]);
 
 	return (
 		<Fragment>
 			<Container className="profile-container">
-				<Row className="nft-display-rows mb-5 pb-3">
-					{/*  */}
-					{editMode === false ? (
-						<Col lg={3} md={6} className="mt-3">
-							<Col md={6} lg={12}>
-								<Image
-									className="profile-image "
-									src={user ? user.profile_image : null}
-									width="250px"
-									height="250px"></Image>
-							</Col>
-							<Col md={6} lg={12} className=" mt-3">
-								<span className="text-majesti text-white user-profile-name font-secondary ">
-									{' '}
-									{user ? '@' + user.display_name : '@displayname'}{' '}
-								</span>{' '}
-							</Col>
-							<Col md={6} lg={12} className="mt-1 ml-1">
-								<span className="text-primary text-center">
-									{user
-										? user.wallet_id.slice(0, 3) +
-										  '...' +
-										  user.wallet_id.slice(-3)
-										: null}
-								</span>
-							</Col>
-							<Col md={6} lg={12} className="mt-4 ml-1">
-								<p className="text-start text-white user-profile-bio">
-									{' '}
-									{user ? user.bio : null}{' '}
-								</p>
-							</Col>
-							<Col md={6} lg={12} className="text-start">
-								<Button
-									className="btn-regal mt-2 mb-3 mr-1"
-									onClick={() => setEditMode(!editMode)}>
-									Edit
-								</Button>
-								<Button className="btn-regal mt-2 mb-3 ml-1 mr-1">
-									<i className="fas fa-cog"></i>
-								</Button>
-							</Col>
+				{user && nft && (
+					<Row className="profile-details-row">
+						<Col md={4} sm={12} className="profile-card-col mx-auto">
+							{/* <Image className="profile-image" src={user.profile_image}></Image> */}
+							<ProfileCard
+								_id={user._id}
+								profile_image={user.profile_image}
+								wallet_id={user.wallet_id}
+								profile_featured_id={user.profile_featured_id}
+								bio={user.bio}
+								profile_bg_color={user.profile_bg_color}
+								display_name={user.display_name}></ProfileCard>
 						</Col>
-					) : (
-						<Col lg={6}  className="">
-							<Form>
-								<Form.Group>
-									<Col lg={12}>
-										<Image
-											className="profile-image mb-2 "
-											src={
-												userChanges.profile_image.length > 1
-													? userChanges.profile_image
-													: user.profile_image
-											}
-											width="250px"
-											height="250px"></Image>
-
-										<Form.File
-											id="exampleFormControlFile1"
-											className="choose-file-button"
-											label={`PROFILE PICTURE UPLOAD (4mb max)`}
-											onChange={(e) => handleFileUpload(e.target.files[0])}
-										/>
-										<Form.Label className="text-white"></Form.Label>
-									</Col>
-								</Form.Group>
-								<Form.Group controlId="formDisplayName">
-									<Col md={12} className="mt-3">
-										<Form.Label className="text-white">DISPLAY NAME</Form.Label>
-										<Form.Control
-											type="text"
-											placeholder={user ? user.display_name : '@displayname'}
-											onChange={(e) => handleDisplayName(e)}
-										/>
-									</Col>
-								</Form.Group>
-								<Form.Group controlId="formWalletId">
-									<Col md={12} className="mt-1">
-										<span className="text-primary">
-											{user
-												? user.wallet_id.slice(0, 3) +
-												  '...' +
-												  user.wallet_id.slice(-3)
-												: null}
-										</span>
-									</Col>
-								</Form.Group>
-								<Form.Group controlId="formBio">
-									<Col lg={12} md={12} className="mt-1">
-										<Form.Label className="text-white">ARTIST BIO</Form.Label>
-										<Form.Control
-											as="textarea"
-											rows={3}
-											type="text"
-											placeholder={user ? user.bio : null}
-											onChange={(e) => handleBio(e)}
-										/>
-									</Col>
-								</Form.Group>
-								<Col lg={12} md={12} className="text-start">
-									<div>
-										<Button
-											className="btn-regal mt-2 mb-3 mr-1"
-											onClick={(e) => handleUpdateUser(e)}>
-											{submitting ? (
-												<span className="spinner-border spinner-border-sm mr-2 mb-1"></span>
-											) : null}
-											{submitting ? 'Submitting' : 'Submit'}
-										</Button>
-										<Button
-											className="btn-regal mt-2 mb-3 ml-1"
-											onClick={() => setEditMode(!editMode)}>
-											CANCEL
-										</Button>
-									</div>
-								</Col>
-							</Form>
+						<Col className="profile-featured-col mx-auto my-5">
+							<NftDisplay
+								_id={nft._id}
+								likes={nft.likes}
+								thumbnail_image={nft.thumbnail_image}
+								nft_id={nft.nft_id}
+								current_bid={nft.current_bid}
+								title={nft.title}
+								auction_startDate={nft.auction_startDate}
+								auction_duration={nft.auction_duration}
+								creator={nft.creator}
+								date_mint={nft.date_mint}
+								tags={nft.tags}
+							/>
 						</Col>
-					)}
-					{/*  */}
-					<Col className="featured-nft" lg={9} md={6}>
-						<div className="text-white font-primary ">Featured</div>
-						<ProfileNftDisplay
-							likes={nfts[5].likes}
-							comments={nfts[5].comments}
-							image={nfts[5].image}
-							id={nfts[5].id}
-							bid={nfts[5].bid}
-							title={nfts[5].title}
-							creator={nfts[5].creator}
-							date_mint={nfts[5].date_mint}
-							current={nfts[5].current}
-							previous={nfts[5].previous}
-						/>
-					</Col>
-				</Row>
-				<Row className="mb-5">
-					<span className="text-white font-secondary">Collection</span>
-					{getAllNfts.length &&
-						getAllNfts.map((nfts, index) => (
-							<Col
-								lg={4}
-								md={3}
-								sm={6}
-								key={index}
-								className="nft-collection mt-3 mb-3">
-								<ProfileNftDisplay
-									image={nfts.thumbnail_image}
-									nft_id={nfts.nft_id}
-								/>
-							</Col>
-						))}
-				</Row>
+					</Row>
+				)}
 				<Row>
-					<span className="text-white font-secondary pt-2">Liked</span>
-					{nfts.length &&
-						nfts.map((nfts, index) => (
-							<Col lg={3} md={3} sm={12} key={index} className="nft-liked mt-3 mb-3">
-								<ProfileNftDisplay
-									likes={nfts.likes}
-									comments={nfts.comments}
-									image={nfts.image}
-									id={nfts.id}
-									bid={nfts.bid}
-									title={nfts.title}
-									creator={nfts.creator}
-									date_mint={nfts.date_mint}
-									current={nfts.current}
-									previous={nfts.previous}
-								/>
-							</Col>
-						))}
-				</Row>
-				<Row>
-					<span className="text-white font-secondary pt-5">Recently Viewed</span>
-					{nfts.length &&
-						nfts.map((nfts, index) => (
-							<Col lg={3} md={3} sm={12} key={index} className="nft-viewed mt-3 mb-3">
-								<ProfileNftDisplay
-									likes={nfts.likes}
-									comments={nfts.comments}
-									image={nfts.image}
-									id={nfts.id}
-									bid={nfts.bid}
-									title={nfts.title}
-									creator={nfts.creator}
-									date_mint={nfts.date_mint}
-									current={nfts.current}
-									previous={nfts.previous}
-								/>
-							</Col>
-						))}
+					{user && user.collections.length > 1
+						? user.collections.map((nfts, i) => {
+								return (
+									<Col
+										lg={3}
+										md={4}
+										sm={6}
+										xs={12}
+										key={index}
+										className="explore-live-feed">
+										<NftDisplay
+											_id={nft._id}
+											likes={nft.likes}
+											thumbnail_image={nft.thumbnail_image}
+											nft_id={nft.nft_id}
+											current_bid={nft.current_bid}
+											title={nft.title}
+											auction_startDate={nft.auction_startDate}
+											auction_duration={nft.auction_duration}
+											creator={nft.creator}
+											date_mint={nft.date_mint}
+											tags={nft.tags}
+										/>
+									</Col>
+								);
+						  })
+						: null}
+					<span className="text-white font-tertiary">
+						Collection <i className="fas fa-plus pl-2"></i>
+					</span>
+					<Col></Col>
+					<span className="text-white font-tertiary">Liked</span>
+					<Col></Col>
+					<span className="text-white font-tertiary">Recently Viewed</span>
+					<Col></Col>
 				</Row>
 			</Container>
 		</Fragment>
@@ -427,3 +303,217 @@ const Profile = (props, web3) => {
 };
 
 export default observer(Profile);
+
+// <Row className="nft-display-rows mb-5 pb-3">
+// {editMode === false ? (
+// 	<Col lg={3} md={6} className="mt-3">
+// 		<Col md={6} lg={12}>
+// 			<Image
+// 				className="profile-image "
+// 				src={user ? user.profile_image : null}
+// 				width="250px"
+// 				height="250px"></Image>
+// 		</Col>
+// 		<Col md={6} lg={12} className=" mt-3">
+// 			<span className="text-majesti text-white user-profile-name font-secondary ">
+// 				{' '}
+// 				{user ? '@' + user.display_name : '@displayname'}{' '}
+// 			</span>{' '}
+// 		</Col>
+// 		<Col md={6} lg={12} className="mt-1 ml-1">
+// 			<span className="text-primary text-center">
+// 				{user
+// 					? user.wallet_id.slice(0, 3) +
+// 					  '...' +
+// 					  user.wallet_id.slice(-3)
+// 					: null}
+// 			</span>
+// 		</Col>
+// 		<Col md={6} lg={12} className="mt-4 ml-1">
+// 			<p className="text-start text-white user-profile-bio">
+// 				{' '}
+// 				{user ? user.bio : null}{' '}
+// 			</p>
+// 		</Col>
+// 		<Col md={6} lg={12} className="text-start">
+// 			<Button
+// 				className="btn-regal mt-2 mb-3 mr-1"
+// 				onClick={() => setEditMode(!editMode)}>
+// 				Edit
+// 			</Button>
+// 			<Button className="btn-regal mt-2 mb-3 ml-1 mr-1">
+// 				<i className="fas fa-cog"></i>
+// 			</Button>
+// 		</Col>
+// 	</Col>
+// ) : (
+// 	<Col lg={6} className="">
+// 		<Form>
+// 			<Form.Group>
+// 				<Col lg={12}>
+// 					<Image
+// 						className="profile-image mb-2 "
+// 						src={
+// 							userChanges.profile_image.length > 1
+// 								? userChanges.profile_image
+// 								: user.profile_image
+// 						}
+// 						width="250px"
+// 						height="250px"></Image>
+
+// 					<Form.File
+// 						id="exampleFormControlFile1"
+// 						className="choose-file-button"
+// 						label={`PROFILE PICTURE UPLOAD (4mb max)`}
+// 						onChange={(e) =>
+// 							handleFileUpload(e.target.files[0])
+// 						}
+// 					/>
+// 					<Form.Label className="text-white"></Form.Label>
+// 				</Col>
+// 			</Form.Group>
+// 			<Form.Group controlId="formDisplayName">
+// 				<Col md={12} className="mt-3">
+// 					<Form.Label className="text-white">
+// 						DISPLAY NAME
+// 					</Form.Label>
+// 					<Form.Control
+// 						type="text"
+// 						placeholder={
+// 							user ? user.display_name : '@displayname'
+// 						}
+// 						onChange={(e) => handleDisplayName(e)}
+// 					/>
+// 				</Col>
+// 			</Form.Group>
+// 			<Form.Group controlId="formWalletId">
+// 				<Col md={12} className="mt-1">
+// 					<span className="text-primary">
+// 						{user
+// 							? user.wallet_id.slice(0, 3) +
+// 							  '...' +
+// 							  user.wallet_id.slice(-3)
+// 							: null}
+// 					</span>
+// 				</Col>
+// 			</Form.Group>
+// 			<Form.Group controlId="formBio">
+// 				<Col lg={12} md={12} className="mt-1">
+// 					<Form.Label className="text-white">
+// 						ARTIST BIO
+// 					</Form.Label>
+// 					<Form.Control
+// 						as="textarea"
+// 						rows={3}
+// 						type="text"
+// 						placeholder={user ? user.bio : null}
+// 						onChange={(e) => handleBio(e)}
+// 					/>
+// 				</Col>
+// 			</Form.Group>
+// 			<Col lg={12} md={12} className="text-start">
+// 				<div>
+// 					<Button
+// 						className="btn-regal mt-2 mb-3 mr-1"
+// 						onClick={(e) => handleUpdateUser(e)}>
+// 						{submitting ? (
+// 							<span className="spinner-border spinner-border-sm mr-2 mb-1"></span>
+// 						) : null}
+// 						{submitting ? 'Submitting' : 'Submit'}
+// 					</Button>
+// 					<Button
+// 						className="btn-regal mt-2 mb-3 ml-1"
+// 						onClick={() => setEditMode(!editMode)}>
+// 						CANCEL
+// 					</Button>
+// 				</div>
+// 			</Col>
+// 		</Form>
+// 	</Col>
+// )}
+// {/*  */}
+// <Col className="featured-nft" lg={9} md={6}>
+// 	<div className="text-white font-primary ">Featured</div>
+// 	<ProfileNftDisplay
+// 		likes={nfts[5].likes}
+// 		comments={nfts[5].comments}
+// 		image={nfts[5].image}
+// 		id={nfts[5].id}
+// 		bid={nfts[5].bid}
+// 		title={nfts[5].title}
+// 		creator={nfts[5].creator}
+// 		date_mint={nfts[5].date_mint}
+// 		current={nfts[5].current}
+// 		previous={nfts[5].previous}
+// 	/>
+// </Col>
+// </Row>
+// <Row className="mb-5">
+// <span className="text-white font-secondary">Collection</span>
+// {getAllNfts.length &&
+// 	getAllNfts.map((nfts, index) => (
+// 		<Col
+// 			lg={4}
+// 			md={3}
+// 			sm={6}
+// 			key={index}
+// 			className="nft-collection mt-3 mb-3">
+// 			<ProfileNftDisplay
+// 				image={nfts.thumbnail_image}
+// 				nft_id={nfts.nft_id}
+// 			/>
+// 		</Col>
+// 	))}
+// </Row>
+// <Row>
+// <span className="text-white font-secondary pt-2">Liked</span>
+// {nfts.length &&
+// 	nfts.map((nfts, index) => (
+// 		<Col
+// 			lg={3}
+// 			md={3}
+// 			sm={12}
+// 			key={index}
+// 			className="nft-liked mt-3 mb-3">
+// 			<ProfileNftDisplay
+// 				likes={nfts.likes}
+// 				comments={nfts.comments}
+// 				image={nfts.image}
+// 				id={nfts.id}
+// 				bid={nfts.bid}
+// 				title={nfts.title}
+// 				creator={nfts.creator}
+// 				date_mint={nfts.date_mint}
+// 				current={nfts.current}
+// 				previous={nfts.previous}
+// 			/>
+// 		</Col>
+// 	))}
+// </Row>
+// <Row>
+// <span className="text-white font-secondary pt-5">
+// 	Recently Viewed
+// </span>
+// {nfts.length &&
+// 	nfts.map((nfts, index) => (
+// 		<Col
+// 			lg={3}
+// 			md={3}
+// 			sm={12}
+// 			key={index}
+// 			className="nft-viewed mt-3 mb-3">
+// 			<ProfileNftDisplay
+// 				likes={nfts.likes}
+// 				comments={nfts.comments}
+// 				image={nfts.image}
+// 				id={nfts.id}
+// 				bid={nfts.bid}
+// 				title={nfts.title}
+// 				creator={nfts.creator}
+// 				date_mint={nfts.date_mint}
+// 				current={nfts.current}
+// 				previous={nfts.previous}
+// 			/>
+// 		</Col>
+// 	))}
+// </Row>
